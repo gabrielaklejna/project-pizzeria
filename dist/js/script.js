32ac9bbd5bf2c7a1043ea55ac82004975bbc21ad
Gabriela Klejna <gabriela.rentel@gmail.com>
2022-11-04 20:53:47 +0100

    add .

--- dist/js/script.js
+++ dist/js/script.js
@@ -57,7 +57,11 @@
       thisProduct.id = id; 
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+      thisProduct.getElements();
+      thisProduct.initOrderForm();
     }  
+
     renderInMenu(){
       const thisProduct = this; 
       /*generate HTML based on template*/
@@ -68,7 +72,99 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       /*add element to menu*/
       menuContainer.appendChild(thisProduct.element);
-    };
+    }
+  
+    initAccordion() {
+      const thisProduct = this; 
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      clickableTrigger.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        thisProduct.element.classList.toggle('active');
+        
+        const activeProducts = document.querySelectorAll('.product.active');
+        for(const activeProduct of activeProducts) {
+          if (activeProduct !== thisProduct.element) {
+            activeProduct.classList.remove('active');
+          }
+        }
+      });
+    }
+
+    initOrderForm(){
+      const thisProduct = this;
+      
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+      
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+      
+      // set price to default price
+      let price = thisProduct.data.price;
+      
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+      
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          if(formData[paramId].includes(optionId) && !option.default) {
+            price += option.price;
+          }
+
+          if(!formData[paramId].includes(optionId) && option.default) {
+            price -= option.price;
+          }
+
+          const imageElem = document.querySelector('.' + paramId + '-' + optionId);
+          if(imageElem) {
+            if(formData[paramId].includes(optionId)) {
+              imageElem.classList.add('active');
+            } else {
+              imageElem.classList.remove('active');
+            }
+          }
+
+        }
+      }
+      
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    }
+
+    getElements(){
+      const thisProduct = this;
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
   }
  
   const app = {
@@ -82,10 +178,12 @@
       thisApp.initData();
       thisApp.initMenu();
     },
+
     initData: function(){
       const thisApp = this;
       thisApp.data = dataSource;
     },
+
     initMenu: function(){
       const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
@@ -93,17 +191,7 @@
         new Product(productData, thisApp.data.products[productData]);
       }
     }
-  }
+  };
 
   app.init();
 }
-
-getElements(){
-  const thisProduct = this;
-
-  thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-  thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
-  thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
-  thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
-  thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
-};
